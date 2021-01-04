93c4b117d4da8161e11ab6888802caccb297bb97
Clement Torti <clement.torti@hotmail.fr>
2021-01-04 09:48:34 +0100

    Ajout d'un premier fichier ada MAIN

---
+++ main.adb
@@ -0,0 +1,12 @@
+--
+-- Main ADA file run at app's launch
+--
+
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+with Ada.Float_Text_IO; use Ada.Float_Text_IO;
+
+
+procedure MAIN is
+begin
+end MAIN;
